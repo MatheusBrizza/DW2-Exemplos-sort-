15c6e8dfe393f2f5c2bbffbe3703099a4ea66fe6
MatheusBrizza <matheusbj95@gmail.com>
2022-05-20 21:59:37 -0300

    adicionado um comentário em reduce.js

--- reduce.js
+++ reduce.js
@@ -1,5 +1,5 @@
 let numeros = [10, 20, 30];
-
+// reduce é usado mais para funções matemáticas
 // 0 é o valor default do acumulador
 const resultado = numeros.reduce(
   (acumulador, proximoValor) => acumulador + proximoValor,
